2d6c051fde2167b08e4a3d76e5ef605f7da7f5e5
Deepak Sharma <sharma.deepak8616@gmail.com>
2021-02-18 21:12:54 -0500

    Class1 code

---
+++ src/Class1/MyFile.java
@@ -0,0 +1,44 @@
+package Class1;
+
+public class MyFile {   // MyFile is the Class-name
+
+    /**
+     *  main method
+     *  It is method which is the starting point of java execution.
+     *
+     *  Code that is inside the main method, will be executed.
+     *
+     */
+    public static void main(String[] args) {    // psvm
+        System.out.println("Hello world!");     // command to print in the console. (sout statement)
+        System.out.println("Good bye");           // \n is same as pressing <Enter> key on keyboard
+
+        System.out.println("Hello world!\nGood Bye");   //  Hello World!
+                                                        //  Good Bye
+
+
+        // to have single line comment.
+        // whatever you write after the double slash, it will be treat a comment on the line.
+
+        /*
+            to have
+
+            multiple
+
+            line  comments
+         */
+
+        /**
+         * another way
+         *
+         * to have
+         *
+         * multiple lines
+         *
+         * of comment
+         */
+
+
+    }
+
+}
